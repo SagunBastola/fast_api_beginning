bf2c67b311de2bd0aa39910955a64069325d48fc
Sagun Bastola <sagunbastola34@gmail.com>
2026-06-05 11:38:33 +0545

    main.py

--- fastapi-ecommerce/app/main.py
+++ fastapi-ecommerce/app/main.py
@@ -22,12 +22,20 @@ def root():
 
 
 @app.get("/products")
-def list_products(name: str = Query(default= None,min_length=1,max_length=50,description="search product by name: ")):
+def list_products(name: str = 
+                  Query(default= None,min_length=1,max_length=50,description="search product by name: "),
+                  sort_by_price: bool=Query(default=False,description="sort products by price"),
+                  order : str = Query(default="asc",description="when sort_by_price is true sort by (asc/dsc)"),
+                  limit : int =Query(default=4,description="no of items to be viewed")):
     products=get_all_products()
     if name: 
         needle = name.strip().lower()
         products=[p for p in products if needle in p.get("name").lower()]
         if not products:
             raise HTTPException(status_code=404,detail="product not found")
-        total=len(products)
+    if sort_by_price:
+        reverse =(order == "dsc")
+        products=sorted(products,reverse=reverse,key=lambda p: p.get("price",""))
+    total=len(products)
+    products=products[0:limit]
     return {"total":total,"products":products}
